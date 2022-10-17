bb51350100e474a7f2749565cf9f4bdd096e4505
Raj <varasaniraj5@gmail.com>
2022-10-17 11:37:48 +0530

    Improve Navbar UI - Remove default horizontal spacing in body container

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,11 +2,11 @@ import { ConnectButton } from "web3uikit"
 
 export default function Header() {
   return (
-    <div className="py-5 border-b-2 flex flex-row">
-      <h1 className="py-4 px-4 font-bold text-3xl">
+    <div className="py-3 border-b-2 flex flex-row items-center">
+      <h1 className="py-4 px-4 font-bold text-2xl tigh">
         Decentralized and Verifiable Raffle
       </h1>
-      <div className="ml-auto py-3">
+      <div className="ml-auto">
         <ConnectButton moralisAuth={false} />
       </div>
     </div>
