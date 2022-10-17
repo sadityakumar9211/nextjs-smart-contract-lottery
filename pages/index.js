88be0da80d1cb2d92058e3fd8ec855a201ec3806
Raj <varasaniraj5@gmail.com>
2022-10-17 11:21:29 +0530

    Formate all files according to new config

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,19 @@ import Header from "../components/Header"
 import LotteryEntrance from "../components/LotteryEntrance"
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Smart Contract Lottery</title>
-                <meta
-                    name="description"
-                    content="Smart Contract Lottery is a decentralized verifiable lottery built for EVM compatible chains.."
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            {/* Header / Connect Button / NavBar */}
-            <Header />
-            <LotteryEntrance />
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Smart Contract Lottery</title>
+        <meta
+          name="description"
+          content="Smart Contract Lottery is a decentralized verifiable lottery built for EVM compatible chains.."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Header / Connect Button / NavBar */}
+      <Header />
+      <LotteryEntrance />
+    </div>
+  )
 }
